b4f03d538df88f562c817c8c98917169a0722ea2
erikbayerlein <erik.bayer@icloud.com>
2025-09-16 11:44:08 -0300

    feat: drawing single vanishing ray

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,23 @@
+#include "elements/ray/ray.h"
 #include "engine/engine.h"
 #include <GLFW/glfw3.h>
 
 int main() {
     Engine engine;
+    vector<Ray> rays;
+
+    rays.push_back(Ray(vec2(-1.0f, 0.0f)));
 
     while (engine.isRunning()) {
         engine.run();
+
+        for (auto& ray : rays) {
+            ray.update();
+            ray.draw();
+        }
+
+        glfwSwapBuffers(engine.window);
+        glfwPollEvents();
     }
     
     engine.stop();
