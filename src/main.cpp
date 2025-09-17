8cbd9e41228303414e28708d6325ea67d13c334b
erikbayerlein <erik.bayer@icloud.com>
2025-09-16 21:40:39 -0300

    feat: drawing black hole

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include "elements/black_hole/black_hole.h"
 #include "elements/ray/ray.h"
 #include "engine/engine.h"
 #include <GLFW/glfw3.h>
@@ -5,11 +6,14 @@
 int main() {
     Engine engine;
     vector<Ray> rays;
+    BlackHole black_hole(vec3(0.0f, 0.0f, 0.0f), 8.54e36);
 
     rays.push_back(Ray(vec2(-1.0f, 0.0f)));
 
     while (engine.isRunning()) {
         engine.run();
+
+        black_hole.draw();
 
         for (auto& ray : rays) {
             ray.update();
